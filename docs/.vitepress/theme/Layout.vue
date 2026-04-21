9bc421bdc68af90454b4313adcdc250822ea4536
Simon Isler <mail@simonisler.ch>
2026-04-21 13:59:27 +0200

    Improve tab anchors

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,8 +1,11 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import CustomFooter from './components/CustomFooter.vue'
+import { useTabAnchors } from './useTabAnchors'
 
 const { Layout } = DefaultTheme
+
+useTabAnchors()
 </script>
 
 <template>
